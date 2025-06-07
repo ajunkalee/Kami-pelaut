0a4e165f304b0614c5b66bc79645afbc5a3955f0
ajunkalee <ajunkalee@gmail.com>
2025-06-07 09:02:11 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,7 @@
+package com.kamipelaut;
+
+public class MainActivity {
+    public static void main(String[] args) {
+        System.out.println("Kami Pelaut App v1.0");
+    }
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Cara build:
+1. Buka dengan Android Studio
+2. Sync Gradle
+3. Klik Build > Build APK
+4. APK hasil ada di /app/build/outputs/apk/
